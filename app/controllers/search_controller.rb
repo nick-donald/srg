096904ec59f8773bf795225fef464febde60d499
nick-donald <nickjdonald@gmail.com>
2013-06-15 21:02:11 -0700

    heroku push

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,11 +10,4 @@ class SearchController < ApplicationController
 		@search = Sunspot.search(params[:search])
 		@results = @search.results
 	end
-
-	protected
-		def search(query)
-			Sunspot.search Article, Video do
-				fulltext query
-			end
-		end
 end
